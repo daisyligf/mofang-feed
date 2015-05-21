ebed459cdc28e55a466790d6ed4f17375169b03d
zhaodexin <milo@192.168.113.93>
2015-05-21 10:38:48 +0800

    add

---
+++ src/main/java/com/mofang/feed/controller/v3/admin/thread/ThreadOpenAction.java
@@ -0,0 +1,61 @@
+package com.mofang.feed.controller.v3.admin.thread;
+
+import org.json.JSONObject;
+
+import com.mofang.feed.controller.AbstractActionExecutor;
+import com.mofang.feed.global.ResultValue;
+import com.mofang.feed.global.ReturnCode;
+import com.mofang.feed.global.ReturnMessage;
+import com.mofang.feed.logic.admin.FeedThreadLogic;
+import com.mofang.feed.logic.admin.impl.FeedThreadLogicImpl;
+import com.mofang.framework.util.StringUtil;
+import com.mofang.framework.web.server.annotation.Action;
+import com.mofang.framework.web.server.reactor.context.HttpRequestContext;
+
+/**
+ * 
+ * @author zhaodx
+ *
+ */
+@Action(url = "feed/v2/backend/thread/open")
+public class ThreadOpenAction extends AbstractActionExecutor
+{
+	private FeedThreadLogic logic = FeedThreadLogicImpl.getInstance();
+
+	@Override
+	protected ResultValue exec(HttpRequestContext context) throws Exception
+	{
+		ResultValue result = new ResultValue();
+		String strOperatorId = context.getParameters("uid");
+		if(!StringUtil.isLong(strOperatorId))
+		{
+			result.setCode(ReturnCode.CLIENT_REQUEST_LOST_NECESSARY_PARAMETER);
+			result.setMessage(ReturnMessage.CLIENT_REQUEST_LOST_NECESSARY_PARAMETER);
+			return result;
+		}
+		
+		String postData = context.getPostData();
+		if(StringUtil.isNullOrEmpty(postData))
+		{
+			result.setCode(ReturnCode.CLIENT_REQUEST_LOST_NECESSARY_PARAMETER);
+			result.setMessage(ReturnMessage.CLIENT_REQUEST_LOST_NECESSARY_PARAMETER);
+			return result;
+		}
+		
+		long operatorId = Long.parseLong(strOperatorId);
+		JSONObject json = new JSONObject(postData);
+		long threadId = json.optLong("tid", 0L);
+		String reason = json.optString("reason", "管理后台操作");
+		if(threadId <= 0)
+		{
+			result.setCode(ReturnCode.CLIENT_REQUEST_DATA_IS_INVALID);
+			result.setMessage(ReturnMessage.CLIENT_REQUEST_DATA_IS_INVALID);
+			return result;
+		}
+		
+		logic.open(threadId, operatorId, reason);
+		result.setCode(ReturnCode.SUCCESS);
+		result.setMessage(ReturnMessage.SUCCESS);
+		return result;
+	}
+}
