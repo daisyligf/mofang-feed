ff40b104ecb2ee8a2a74ec33e322feee5e1adc4d
linjiaxing <linjiaxing@mofang.com>
2015-05-30 13:27:36 +0800

    #update

---
+++ src/main/java/com/mofang/feed/controller/v3/front/web/user/UserSetProhibitAction.java
@@ -0,0 +1,52 @@
+package com.mofang.feed.controller.v3.front.web.user;
+
+import org.json.JSONObject;
+
+import com.mofang.feed.controller.AbstractActionExecutor;
+import com.mofang.feed.global.ResultValue;
+import com.mofang.feed.global.ReturnCode;
+import com.mofang.feed.global.ReturnMessage;
+import com.mofang.feed.logic.FeedUserLogic;
+import com.mofang.feed.logic.impl.FeedUserLogicImpl;
+import com.mofang.framework.util.StringUtil;
+import com.mofang.framework.web.server.annotation.Action;
+import com.mofang.framework.web.server.reactor.context.HttpRequestContext;
+
+@Action(url = "feed/v2/user/setprohibit")
+public class UserSetProhibitAction extends AbstractActionExecutor {
+
+	private FeedUserLogic logic = FeedUserLogicImpl.getInstance();
+	
+	@Override
+	protected ResultValue exec(HttpRequestContext context) throws Exception {
+		ResultValue result = new ResultValue();
+		String strOperatorId = context.getParameters("uid");
+		if(!StringUtil.isLong(strOperatorId))
+		{
+			result.setCode(ReturnCode.CLIENT_REQUEST_LOST_NECESSARY_PARAMETER);
+			result.setMessage(ReturnMessage.CLIENT_REQUEST_LOST_NECESSARY_PARAMETER);
+			return result;
+		}
+
+		String postData = context.getPostData();
+		if(StringUtil.isNullOrEmpty(postData))
+		{
+			result.setCode(ReturnCode.CLIENT_REQUEST_LOST_NECESSARY_PARAMETER);
+			result.setMessage(ReturnMessage.CLIENT_REQUEST_LOST_NECESSARY_PARAMETER);
+			return result;
+		}
+		
+		long operatorId = Long.parseLong(strOperatorId);
+		JSONObject json = new JSONObject(postData);
+		long forumId = json.optLong("fid", 0L);
+		long userId = json.optLong("user_id", 0L);
+		if(forumId <= 0 || userId <= 0)
+		{
+			result.setCode(ReturnCode.CLIENT_REQUEST_DATA_IS_INVALID);
+			result.setMessage(ReturnMessage.CLIENT_REQUEST_DATA_IS_INVALID);
+			return result;
+		}
+		return logic.setProhibit(forumId, userId, operatorId);
+	}
+
+}
