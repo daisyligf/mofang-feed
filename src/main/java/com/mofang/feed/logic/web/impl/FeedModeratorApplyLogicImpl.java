fbef5f1ec7ef1869279f1a51a32b25df9db60da4
zhaodexin <milo@192.168.113.93>
2015-06-03 14:16:56 +0800

    update

--- src/main/java/com/mofang/feed/logic/web/impl/FeedModeratorApplyLogicImpl.java
+++ src/main/java/com/mofang/feed/logic/web/impl/FeedModeratorApplyLogicImpl.java
@@ -1,9 +1,11 @@
 package com.mofang.feed.logic.web.impl;
 
+import org.json.JSONObject;
+
 import com.mofang.feed.global.ResultValue;
 import com.mofang.feed.global.ReturnCode;
 import com.mofang.feed.global.ReturnMessage;
-import com.mofang.feed.logic.FeedModeratorApplyLogic;
+import com.mofang.feed.logic.web.FeedModeratorApplyLogic;
 import com.mofang.feed.model.FeedModeratorApply;
 import com.mofang.feed.model.ModeratorApplyCondition;
 import com.mofang.feed.service.FeedModeratorApplyService;
@@ -29,16 +31,27 @@ public class FeedModeratorApplyLogicImpl implements FeedModeratorApplyLogic
 		{
 			ResultValue result = new ResultValue();
 			ModeratorApplyCondition condition = applyService.checkCondition(model.getUserId(), model.getForumId(), false);
+			boolean isPass = false;
 			if(condition.isFollowForumIsOK() && condition.isThreadsIsOK() && condition.isTimeIntervalIsOK() && condition.isTopEliteCountIsOK())
 			{
 				applyService.add(model);
+				isPass = true;
 				result.setCode(ReturnCode.SUCCESS);
 				result.setMessage(ReturnMessage.SUCCESS);
-				return result;
+			}
+			else
+			{
+				result.setCode(ReturnCode.MODERATOR_APPLY_CONDITION_INSUFFICIENT);
+				result.setMessage(ReturnMessage.MODERATOR_APPLY_CONDITION_INSUFFICIENT);
 			}
 			
-			result.setCode(ReturnCode.MODERATOR_APPLY_CONDITION_INSUFFICIENT);
-			result.setMessage(ReturnMessage.MODERATOR_APPLY_CONDITION_INSUFFICIENT);
+			JSONObject data = new JSONObject();
+			data.put("is_pass", isPass);
+			data.put("is_follow_ok", condition.isFollowForumIsOK());
+			data.put("is_threads_ok", condition.isThreadsIsOK());
+			data.put("is_interval_ok", condition.isTimeIntervalIsOK());
+			data.put("is_elitecount_ok", condition.isTopEliteCountIsOK());
+			result.setData(data);
 			return result;
 		}
 		catch (Exception e)
