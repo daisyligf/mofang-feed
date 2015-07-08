d3cac06130115021b09b16b680e32dbd69e4fa41
zhaodexin <milo@192.168.104.46>
2015-07-08 12:23:40 +0800

    update

--- src/main/java/com/mofang/feed/mysql/impl/FeedPostDaoImpl.java
+++ src/main/java/com/mofang/feed/mysql/impl/FeedPostDaoImpl.java
@@ -234,7 +234,7 @@ public class FeedPostDaoImpl extends AbstractMysqlSupport<FeedPost> implements F
 	{
 		StringBuilder strSql = new StringBuilder();
 		strSql.append("select source_id, type from( ");
-		strSql.append("select post_id as source_id, '1' as type, create_time from feed_post where user_id = " + userId + " and status = 1 ");
+		strSql.append("select post_id as source_id, '1' as type, create_time from feed_post where user_id = " + userId + " and status = 1 and position > 1 ");
 		strSql.append("union all ");
 		strSql.append("select comment_id as source_id, '2' as type, create_time from feed_comment where user_id = " + userId + " and status = 1 ");
 		strSql.append(") a order by create_time desc ");
@@ -264,7 +264,7 @@ public class FeedPostDaoImpl extends AbstractMysqlSupport<FeedPost> implements F
 	{
 		StringBuilder strSql = new StringBuilder();
 		strSql.append("select count(1) from( ");
-		strSql.append("select post_id as source_id from feed_post where user_id = " + userId + " and status = 1 ");
+		strSql.append("select post_id as source_id from feed_post where user_id = " + userId + " and status = 1 and position > 1 ");
 		strSql.append("union all ");
 		strSql.append("select comment_id as source_id from feed_comment where user_id = " + userId + " and status = 1 ");
 		strSql.append(") a ");
