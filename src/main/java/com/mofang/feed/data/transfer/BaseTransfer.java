7056d5de617d2b7fd292e87831f45a41d7b2e4ee
zhaodexin <milo@192.168.104.46>
2015-07-15 16:37:46 +0800

    update

--- src/main/java/com/mofang/feed/data/transfer/BaseTransfer.java
+++ src/main/java/com/mofang/feed/data/transfer/BaseTransfer.java
@@ -1,7 +1,6 @@
 package com.mofang.feed.data.transfer;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
@@ -12,16 +11,11 @@ import java.sql.ResultSet;
  */
 public abstract class BaseTransfer
 {
-	private final static String Driver = "com.mysql.jdbc.Driver";
-	
 	public ResultSet getData(String strSql)
 	{
 		try
 		{
-			Class.forName(Driver);
-			//String url="jdbc:mysql://192.168.1.61:3306/feed_ol?user=root&password=mofang888&useUnicode=true&characterEncoding=utf-8&autoReconnect=true&failOverReadOnly=false";
-			//String url="jdbc:mysql://10.6.16.194:3306/feed?user=feedbbs&password=UcCCTzAe&useUnicode=true&characterEncoding=utf-8&autoReconnect=true&failOverReadOnly=false";
-			//Connection conn = DriverManager.getConnection(url);
+			//Class.forName(Driver);
 			Connection conn = MysqlConnectionProvider.ORIGINAL_CONN;
 			PreparedStatement pstmt = conn.prepareStatement(strSql);
 			ResultSet rs = pstmt.executeQuery();
@@ -38,10 +32,7 @@ public abstract class BaseTransfer
 	{
 		try
 		{
-			Class.forName(Driver);
-			//String url="jdbc:mysql://192.168.1.61:3306/feed?user=root&password=mofang888&useUnicode=true&characterEncoding=utf-8&autoReconnect=true&failOverReadOnly=false";
-			//String url="jdbc:mysql://10.6.16.194:3306/feed_v3?user=mofang&password=UcCCTzAe&useUnicode=true&characterEncoding=utf-8&autoReconnect=true&failOverReadOnly=false";
-			//Connection conn = DriverManager.getConnection(url);
+			//Class.forName(Driver);
 			Connection conn = MysqlConnectionProvider.NEW_CONN;
 			PreparedStatement pstmt = conn.prepareStatement(strSql);
 			pstmt.execute();
@@ -56,10 +47,7 @@ public abstract class BaseTransfer
 	{
 		try
 		{
-			Class.forName(Driver);
-			//String url="jdbc:mysql://192.168.1.61:3306/feed?user=root&password=mofang888&useUnicode=true&characterEncoding=utf-8&autoReconnect=true&failOverReadOnly=false";
-			//String url="jdbc:mysql://10.6.16.194:3306/feed_v3?user=mofang&password=UcCCTzAe&useUnicode=true&characterEncoding=utf-8&autoReconnect=true&failOverReadOnly=false";
-			//Connection conn = DriverManager.getConnection(url);
+			//Class.forName(Driver);
 			Connection conn = MysqlConnectionProvider.NEW_CONN;
 			PreparedStatement pstmt = conn.prepareStatement(strSql);
 			return pstmt.executeQuery();
